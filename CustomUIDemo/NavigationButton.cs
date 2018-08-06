c1ca12bec90a496bac58efb0ff10d9a43bac6b6b
Barry <wjp_barry@hotmail.com>
2018-08-06 17:53:24 +0800

    Create the Visual animation for the control. Need to finish the items restyle.

---
+++ CustomUIDemo/NavigationButton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace CustomUIDemo
+{
+    public sealed class NavigationButton : ContentControl
+    {
+        private Ellipse _backgroundElement;
+        public event RoutedEventHandler Click;
+        public NavigationButton()
+        {
+            this.DefaultStyleKey = typeof(NavigationButton);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            _backgroundElement = GetTemplateChild("BackgroundElement") as Ellipse;
+            base.OnApplyTemplate();
+        }
+
+        protected override void OnTapped(TappedRoutedEventArgs e)
+        {
+            Click?.Invoke(this, new RoutedEventArgs());
+            base.OnTapped(e);
+        }
+        }
+}
--- CustomUIDemo/RadialMenu.cs
+++ CustomUIDemo/RadialMenu.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation.Metadata;
+using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Markup;
 using Windows.UI.Xaml.Media;
@@ -23,7 +26,15 @@ namespace CustomUIDemo
         #region fields
         private RadialMenuItemCollection _items;
         private RadialMenuItemPresenter _currentItemPresenter;
+        private Grid _contentGrid;
+        internal NavigationButton _navigationButtion;
+        #endregion
 
+        #region animationready
+        Visual _contentGridVisual;
+        Compositor _compositor;
+        ScalarKeyFrameAnimation rotationAnimation;
+        Vector3KeyFrameAnimation scaleAnimation;
         #endregion
 
         #region IRadialMenuItemsControl
@@ -34,6 +45,7 @@ namespace CustomUIDemo
             {
                 return _items;
             }
+   
         }
         #endregion
         //public IRadialMenuItemsControl CurrentItem
@@ -58,7 +70,36 @@ namespace CustomUIDemo
 
         //public event DependencyPropertyChangedEventHandler CurrentItemChanged;
 
+        #region DP
+        public bool IsExpanded
+        {
+            get { return (bool)GetValue(IsExpandedProperty); }
+            set { SetValue(IsExpandedProperty, value); }
+        }
 
+        // Using a DependencyProperty as the backing store for IsExpanded.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsExpandedProperty =
+            DependencyProperty.Register("IsExpanded", typeof(bool), typeof(RadialMenu), new PropertyMetadata(true, new PropertyChangedCallback(OnIsExpandedChanged)));
+        #endregion
+
+        private static void OnIsExpandedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as RadialMenu).IsExpandedChanged();
+        }
+
+        private void IsExpandedChanged()
+        {        
+            if (IsExpanded)
+            {
+                Expand(); 
+            }
+            else
+            {
+
+                Collapse();
+            }
+           
+        }
 
         public RadialMenu()
         {
@@ -69,8 +110,60 @@ namespace CustomUIDemo
 
         protected override void OnApplyTemplate()
         {
-
+            _contentGrid = GetTemplateChild("ContentGrid") as Grid;
+            _navigationButtion = GetTemplateChild("NavigationButton") as NavigationButton;
+            _navigationButtion.Click += _navigationButtion_Click;
+            PrepareAnimation();
             base.OnApplyTemplate();
+        }
+
+        private void _navigationButtion_Click(object sender, RoutedEventArgs e)
+        {
+            IsExpanded = !IsExpanded;
+        }
+
+        void PrepareAnimation()
+        {
+            _contentGridVisual = ElementCompositionPreview.GetElementVisual(_contentGrid);
+            _compositor = _contentGridVisual.Compositor;
+            rotationAnimation = _compositor.CreateScalarKeyFrameAnimation();
+            scaleAnimation = _compositor.CreateVector3KeyFrameAnimation();
+            var easing = _compositor.CreateLinearEasingFunction();
+            _contentGrid.SizeChanged += (s, e) =>
+            {
+                _contentGridVisual.CenterPoint = new Vector3((float)_contentGrid.ActualWidth / 2.0f, (float)_contentGrid.ActualHeight / 2.0f, 0);
+            };
+            scaleAnimation.InsertKeyFrame(0.0f, new Vector3() { X = 0.0f, Y = 0.0f, Z = 0.0f });
+            scaleAnimation.InsertKeyFrame(1.0f, new Vector3() { X = 1.0f, Y = 1.0f, Z = 0.0f }, easing);
+
+            rotationAnimation.InsertKeyFrame(0.0f, -90.0f);
+            rotationAnimation.InsertKeyFrame(1.0f, 0.0f, easing);
+        }
+
+        void Expand()
+        {
+            if (_contentGridVisual != null)
+            {
+                scaleAnimation.Direction = AnimationDirection.Normal;
+                rotationAnimation.Direction = AnimationDirection.Normal;
+                scaleAnimation.Duration = TimeSpan.FromSeconds(0.2);
+                rotationAnimation.Duration = TimeSpan.FromSeconds(0.2);            
+                _contentGridVisual.StartAnimation(nameof(_contentGridVisual.Scale), scaleAnimation);
+                _contentGridVisual.StartAnimation(nameof(_contentGridVisual.RotationAngleInDegrees), rotationAnimation);
+            }
+        }
+
+        void Collapse()
+        {
+            if (_contentGridVisual != null)
+            {
+                scaleAnimation.Direction = AnimationDirection.Reverse;
+                rotationAnimation.Direction = AnimationDirection.Reverse;
+                scaleAnimation.Duration = TimeSpan.FromSeconds(0.2);
+                rotationAnimation.Duration = TimeSpan.FromSeconds(0.2);
+                _contentGridVisual.StartAnimation(nameof(_contentGridVisual.Scale), scaleAnimation);
+                _contentGridVisual.StartAnimation(nameof(_contentGridVisual.RotationAngleInDegrees), rotationAnimation);
+            }
         }
     }
 
